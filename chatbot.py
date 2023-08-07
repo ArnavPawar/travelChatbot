65b7500dd323d2096353bbdbadc1e60e3572ae12
Arnav Pawar <arnav.ap.pawar@gmail.com>
2023-08-07 12:36:27 -0400

    cahtbot adding
    Working python cahtbot

--- chatbot.py
+++ chatbot.py
@@ -1,27 +1,30 @@
 import openai
+import requests
 
 # Configure OpenAI
-openai.api_key = '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'  
+openai.api_key = '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'  
+weather_api_key = '1c223b1c1c544d91831152821231807'
 
-# Chatbot Function
-def chat_with_bot(user_input):
+import openai
+
+
+
+def chat_with_bot(prompt):
     response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=user_input,
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.7
+        engine="text-davinci-003",  # You can experiment with different engines
+        prompt=prompt,
+        max_tokens=50  # You can adjust the response length
     )
-
     return response.choices[0].text.strip()
 
-# Main Loop
-while True:
-    user_input = input("User: ")
+print("Chatbot: Hello! I'm your friendly chatbot. How can I assist you today?")
 
+while True:
+    user_input = input("You: ")
     if user_input.lower() == 'exit':
+        print("Chatbot: Goodbye!")
         break
 
-    bot_response = chat_with_bot(user_input)
+    prompt = f"You: {user_input}\nChatbot:"
+    bot_response = chat_with_bot(prompt)
     print(f"Chatbot: {bot_response}")
